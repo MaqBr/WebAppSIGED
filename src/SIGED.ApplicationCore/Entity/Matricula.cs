880b8b68e4e30a96fe887913da608c56dd92f3c9
MARCIO ALEXANDRE DE QUEIROZ <maqw3@hotmail.com>
2018-04-04 18:15:43 -0400

    Adiciona FKs em Matricula

--- src/SIGED.ApplicationCore/Entity/Matricula.cs
+++ src/SIGED.ApplicationCore/Entity/Matricula.cs
@@ -6,7 +6,9 @@ namespace SIGED.ApplicationCore.Entity
 {
     public class Matricula : BaseEntity
     {
+        public int EstudanteId { get; set; }
         public Estudante Estudante { get; set; }
+        public int CursoId { get; set; }
         public Curso Curso { get; set; }
     }
 }
